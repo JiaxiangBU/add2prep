f0ab969aa954847a48ba4095caba66d865f96c46
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2018-11-13 10:33:13 +0800

    add help doc

--- R/repex_in.R
+++ R/repex_in.R
@@ -1,3 +1,16 @@
+#' Put regular expression on a string and output T/F
+#'
+#' @param x Character. A column of character.
+#' @param regex Character. The regular expression rule defined.
+#'
+#' @return Logical. A column of logical values.
+#' @author Jiaxiang Li \email{alex.lijiaxiang@foxmail.com}
+#'
+#' @import tidyverse
+#'
+#' @seealso str_subset
+#' @export
+
 regex_in <- function(x,regex = '.'){
     x %in% str_subset(x,regex)
 }
